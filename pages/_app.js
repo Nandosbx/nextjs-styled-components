e32e3d5c9eb68faac05f60e1e8bfcf233f9877f3
Fernando <nandosbx@users.noreply.github.com>
2021-07-13 23:21:33 -0300

    First commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,26 +1,32 @@
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
-  body {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+  body {    
+    background-color: #D9E9F9;
+    font-family: sans-serif;
   }
 `
 
 const theme = {
-  colors: {
-    primary: '#0070f3',
-  },
+    colors: {
+        primary: '#0070f3',
+    },
 }
 
 export default function App({ Component, pageProps }) {
-  return (
-    <>
-      <GlobalStyle />
-      <ThemeProvider theme={theme}>
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </>
-  )
+    return (
+        <>
+            <GlobalStyle />
+            <ThemeProvider theme={theme}>
+                <Component {...pageProps} />
+            </ThemeProvider>
+        </>
+    )
 }
